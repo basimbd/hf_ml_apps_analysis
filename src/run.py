b65e5b751987631d574d5f6f60a3ed2369893d4f
basimbd <basimuddintuhin@gmail.com>
2023-11-30 20:53:25 +0600

    create output directory at the start of project and add it to gitignore

--- src/run.py
+++ src/run.py
@@ -1,7 +1,11 @@
-from utils.common_utils import dict_to_csv, PROJECT_DIR
+from utils.common_utils import PROJECT_DIR
 import utils.model_utils as model_utils
 import utils.space_utils as space_utils
 import pandas as pd
+from os import path, makedirs
+
+if not path.exists(f"{PROJECT_DIR}/output"):
+    makedirs(f"{PROJECT_DIR}/output")
 
 
 def get_spaces_df(model_type: str):
@@ -12,17 +16,11 @@ def get_spaces_df(model_type: str):
     return spaces_df
 
 
-def get_space_sizes(model_type: str, spaces_df: pd.DataFrame):
-    spaces_size_dict = space_utils.get_spaces_sizes(list(spaces_df.values))
-    print(f'==========analyzed {model_type} spaces: {len(spaces_size_dict)}==========')
-    dict_to_csv(spaces_size_dict, f"{PROJECT_DIR}/output/{model_type}-space-sizes.csv", ["repo_id", "size"])
-    return spaces_size_dict
-
-
 def get_spaces_and_their_sizes(model_type: str, spaces_df: pd.DataFrame = None):
     if spaces_df is None:
         spaces_df = get_spaces_df(model_type)
     spaces_size_dict = space_utils.save_and_get_spaces_sizes(list(spaces_df.values), model_type)
+    print(f'==========analyzed {model_type} spaces: {len(spaces_size_dict)}==========')
 
     avg_space_size = sum(spaces_size_dict.values())/len(spaces_size_dict)
     print(f'==========Avg {model_type} space size: {avg_space_size}==========')
