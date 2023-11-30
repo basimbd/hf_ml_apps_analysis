e3a324c3bf17de3bd70df5011990e644f6e7141b
basimbd <basimuddintuhin@gmail.com>
2023-11-30 13:47:15 +0600

    add common functionality and suppress git clone print and rm prompt in terminal

--- src/utils/common_utils.py
+++ src/utils/common_utils.py
@@ -1,7 +1,16 @@
 import subprocess
+import pandas as pd
+from os.path import abspath, dirname
 
 HUGGING_FACE_HOST_URL = 'https://huggingface.co'
+PROJECT_DIR = dirname(dirname(dirname(abspath(__file__))))
 
 
 def run_shell_command(command, check=True):
     return subprocess.run(command, shell=True, check=check, stdout=subprocess.PIPE).stdout.decode()
+
+
+def dict_to_csv(dictionary: dict, path: str, columns=None):
+    if columns is None:
+        columns = ["repo_id", "size"]
+    pd.DataFrame(data=dictionary.items(), columns=columns).to_csv(path, sep=",", encoding="utf-8", index=False)
--- src/utils/repo_utils.py
+++ src/utils/repo_utils.py
@@ -1,12 +1,12 @@
 import subprocess
 import platform
-from src.utils.common_utils import HUGGING_FACE_HOST_URL
+from utils.common_utils import HUGGING_FACE_HOST_URL
 
 
 def git_clone(repo_id):
     repo_url = HUGGING_FACE_HOST_URL+f'/spaces/{repo_id}'
     try:
-        subprocess.run(['git', 'clone', repo_url], check=True)
+        subprocess.run(['git', 'clone', '--quiet', repo_url], check=True)
     except subprocess.CalledProcessError as err:
         print(f'Returned: {err.returncode}. Failed to clone repo: {repo_url}')
 
@@ -14,7 +14,7 @@ def git_clone(repo_id):
 def delete_directory(folder):
     try:
         if platform.system() == 'Linux':
-            subprocess.run(['rm', f'{folder}/', '-R'], check=True)
+            subprocess.run(['rm', f'{folder}/', '-rf'], check=True)
         elif platform.system() == 'Windows':
             subprocess.run(['rd', '/s', '/q', f'{folder}'], check=True)
     except subprocess.CalledProcessError as err:
